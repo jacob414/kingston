17e1d4e150d690f6b18dc773ec0cb024be0790e7
Jacob Oscarson <jacob@414soft.com>
2021-01-21 12:49:09 +0100

    Fixed incorrect test

--- kingston/test/test_match_wb.py
+++ kingston/test/test_match_wb.py
@@ -23,17 +23,17 @@ pytestmark = pytest.mark.wbox
 
 @fixture.params(
     "value, pattern, expected",
-    (1, 1, 1),
-    (1, Any, 1),
-    ('x', 1, ()),
-    (1.1, 1, ()),
-    (object(), 1, ()),
-    (TypeMatcher(), 1, ()),
-    ((1,2,3), (1,2,3), (1,2,3)),
+    (1, 1, True),
+    (1, Any, True),
+    ('x', 1, False),
+    (1.1, 1, False),
+    (object(), 1, False),
+    (TypeMatcher(), 1, False),
+    ((1,2,3), (1,2,3), True),
 )  # yapf: disable
 def test_match(value, pattern, expected) -> None:
     "Should match_hit"
-    match(value, pattern) == expected
+    assert match(value, pattern) == expected
 
 
 @fixture.params(
